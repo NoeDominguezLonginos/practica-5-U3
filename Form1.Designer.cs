1e55a2a176aefb72bb92770b71ba8fc54534cccb
LENOVO T450 <LENOVO T450@DESKTOP-S6J3G1K>
2023-10-31 00:26:30 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace practica_5_U3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(171, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(254, 75);
+            label1.TabIndex = 0;
+            label1.Text = "Dado como dato un numero\r\ndeterminar si es par o impar\r\ny divisible por 3";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(378, 159);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(135, 259);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 38);
+            button1.TabIndex = 3;
+            button1.Text = "Determinar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(327, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 38);
+            button2.TabIndex = 4;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(57, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(241, 25);
+            label2.TabIndex = 5;
+            label2.Text = "Ingresa numero cualquiera";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(574, 358);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private FileSystemWatcher fileSystemWatcher1;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+    }
+}
